a33187b821f6d7d6bba25f7cb8a96aa92a1a2f08
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-21 18:48:13 -0400

    initial commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+//= require 'jquery-ui/js/jquery-ui-1.10.2.custom.min.js'
+//= require 'foundation/js/vendor/custom.modernizr.js'
+//= require 'foundation/js/vendor/zepto.js'
+//= require 'foundation/js/foundation.min.js'
+//= require 'underscore-min'
+//= require jquery
+//= require jquery_ujs
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virgingalactic::Application.config.secret_token = 'fdc626ec644413091286bfef8cc96d20e10e5808e58d0240c86a790ca68acd6a6da376a38bfa57e30ce50e018eaedc675b8886c99eb198d6dd05b1c0184b24e0'
