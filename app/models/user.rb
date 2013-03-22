2c26472eb46fcd6e912cf55b2837d7b4836c36cd
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-22 14:02:23 -0400

    modified application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,6 @@
 
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :is_admin
   has_many :seats, :inverse_of => :user
 end
