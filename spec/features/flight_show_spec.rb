fe2f74e9faa901ca59965bb5f080aec8dc6f2dbc
Sean Marzug-McCarthy <samdec11@gmail.com>
2013-03-23 17:13:10 -0400

    adding to tests

--- spec/features/flight_show_spec.rb
+++ spec/features/flight_show_spec.rb
@@ -8,13 +8,25 @@ describe 'Flight Show' do
       page.should have_text('JFK')
       page.should have_text('LAX')
     end
+    it 'shows the seats for a flight' do
+      login_as_regular_user
+      page.should have_css('table tr td .empty')
+    end
   end
-  describe 'POST /'
+  describe 'POST /seats/3/reserve' do
+    it 'reserves the seat for the user' do
+
+    end
+  end
 
   private
   def login_as_regular_user
     user = FactoryGirl.create(:user)
     flight = FactoryGirl.create(:flight)
+    plane = FactoryGirl.create(:plane)
+    flight.plane = plane
+    flight.save
+    flight.create_seats(flight.plane_id)
     visit root_path
     click_link('Login')
     fill_in('Email', :with => user.email)
