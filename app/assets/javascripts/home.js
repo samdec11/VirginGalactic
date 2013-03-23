cbf9d9bf129825739844f32ba95018ab61a63e19
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-23 14:22:45 -0400

    more login progress

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
-    $("div.panel_button").click(
-        function(){ $("div#panel").animate({ height: "500px" }) .animate({
-        height: "400px" }, "fast"); $("div.panel_button").toggle();
-            }); $("div#hide_button").click(function(){
-        $("div#panel").animate({
-            height: "0px"
+  $("div.panel_button").click(
+    function(){
+      $("div#panel").animate({ height: "500px" }).animate({height: "400px" }, "fast");
+      $("div.panel_button").toggle();
+    }
+  );
 
-        }, "fast");
-      });
+  $("div#hide_button").click(
+    function(){
+      $("div#panel").animate({height: "0px"}, "fast");
+    }
+  );
 });
 
 function clear_form() {
