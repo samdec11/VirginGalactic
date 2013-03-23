7c0752c5e53bae00df72b9357ad39dc79d707264
Jillian Youngblood <jillian.youngblood@gmail.com>
2013-03-23 17:40:30 -0400

    it slides

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,18 +1,30 @@
-$(document).ready(function() {
-  $("div.panel_button").click(
-    function(){
-      $("div#panel").animate({ height: "500px" }).animate({height: "400px" }, "fast");
-      $("div.panel_button").toggle();
-    }
-  );
-
-  $("div#hide_button").click(
-    function(){
-      $("div#panel").animate({height: "0px"}, "fast");
-    }
-  );
+$(function(){
+  $("body").on("click", ".panel_button", bounce_down);
+  $("body").on("click", "#hide_button", bounce_hide);
+  $('#create_account').click(slide_form_left);
+  $('#login').click(slide_form_right);
 });
 
 function clear_form() {
   $('#form').empty();
+}
+
+function bounce_down(e){
+  e.preventDefault();
+  $("div#panel").animate({ height: "500px" }).animate({height: "400px" }, "fast");
+  $("div.panel_button").toggle();
+}
+
+function bounce_hide(e){
+  e.preventDefault();
+  $("div#panel").animate({height: "0px"}, "fast");
+}
+
+function slide_form_left(e){
+  e.preventDefault();
+  $('#form_slider').animate({ left: "-775px"});
+}
+function slide_form_right(e){
+  e.preventDefault();
+  $('#form_slider').animate({ left: "0"});
 }
