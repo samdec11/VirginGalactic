a385810cde1d200163447ddc7cda26042e5c27f9
Eric Holland <eric@f-robots.com>
2013-03-24 14:59:27 -0400

    add login_in nav

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -16,5 +16,6 @@ class SessionController < ApplicationController
   def destroy
     session[:user_id] = nil
     authenticate
+    redirect_to(root_path)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def login_nav
+    nav = ""
+
+    if @auth.present?
+
+      if @auth.is_admin?
+        nav += "<li>#{link_to('Planes', planes_path, :class => 'button radius alert')}<li>"
+        nav += "<li>#{link_to('Flights', flights_path, :class => 'button radius alert')}<li>"
+        nav += "<li>#{link_to(@auth.email, login_path, :method => :delete, :class => 'button radius alert')}<li>"
+
+      else
+        nav += "<li>#{link_to('Search', search_path, :class => 'button radius alert')}<li>"
+        nav += "<li>#{link_to(@auth.email, login_path, :method => :delete, :class => 'button radius alert')}<li>"
+      end
+
+    else
+      nav += "<li>#{link_to('Register', new_user_path, :remote => true, :class => 'button radius alert')}</li>"
+      nav += "<li>#{link_to('Login', login_path, :remote => true, :class => 'button radius alert')}</li>"
+    end
+  end
 end
