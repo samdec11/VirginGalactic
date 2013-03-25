77d1d9977168a0bbad667a83d1f466698f61054b
Eric Holland <eric@f-robots.com>
2013-03-24 20:18:28 -0400

    add greeting with user name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,23 @@ module ApplicationHelper
     if @auth.present?
 
       if @auth.is_admin?
-        nav += "<li>#{link_to('Planes', planes_path, :class => 'button radius alert')}</li>"
-        nav += "<li>#{link_to('Flights', flights_path, :class => 'button radius alert')}</li>"
-        nav += "<li>#{link_to(@auth.email, login_path, :method => :delete, :class => 'button radius alert')}</li>"
+        nav += "<li>#{link_to('Planes', planes_path, :class => 'button radius secondary nav')}</li>"
+        nav += "<li>#{link_to('Flights', flights_path, :class => 'button radius secondary nav')}</li>"
+        nav += "<li>#{link_to('Logout', login_path, :method => :delete, :class => 'button radius alert')}</li>"
 
       else
-        nav += "<li>#{link_to('Search', search_path, :class => 'button radius alert')}</li>"
-        nav += "<li>#{link_to(@auth.email, login_path, :method => :delete, :class => 'button radius alert')}</li>"
+        nav += "<li>#{link_to('Search', search_path, :class => 'button radius secondary nav')}</li>"
+        nav += "<li>#{link_to('Logout', login_path, :method => :delete, :class => 'button radius alert')}</li>"
       end
+    end
+  end
+
+  def greeting
+    links = ""
+    if @auth.present?
+      "<li id='greeting'>#{link_to('Welcome back' + ' | ' + @auth.name, '#', :id => 'greeting')}</li>"
+    else
+      links += ""
     end
   end
 end
